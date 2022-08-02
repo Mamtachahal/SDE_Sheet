03061a651db861c42a09c6bf9b1a3d8c8c5e3d39
Mamtachahal <83857458+Mamtachahal@users.noreply.github.com>
2022-08-02 18:55:08 +0530

    Create Searc_2d.cpp

---
+++ Day3/Searc_2d.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int x=INT_MAX,ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(abs(nums[i])<x)
+            {
+                x=abs(nums[i]);
+                ans=nums[i];
+            }
+            else if(abs(nums[i])==x)
+            {
+                ans=max(ans,nums[i]);
+            }
+        }
+        return ans;
+    }
+};
