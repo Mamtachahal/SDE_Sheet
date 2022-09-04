372c46a3a8e868cff7fdf3a258a470fe5b1ab095
Mamta <83857458+Mamtachahal@users.noreply.github.com>
2022-09-04 12:24:52 +0530

    Update setMatrix.cpp

--- Day1/setMatrix.cpp
+++ Day1/setMatrix.cpp
@@ -32,3 +32,59 @@ public:
                 }
     }
 };
+
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+ void setZeroes(vector < vector < int >> & mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(mat[i][0]==0)c=1;
+            for(int j=1;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    mat[i][0]=0;
+                    mat[0][j]=0;
+                }
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=1;j--)
+            {
+                if(mat[i][0]==0 || mat[0][j]==0)
+                {
+                    mat[i][j]=0;
+                }
+            }
+            if(c==1)mat[i][0]=0;
+        }
+ }
+        int main(){
+            int n=3;
+            int m=3;
+             vector<vector<int>> arr(n, vector<int>(m));
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    cin>>arr[i][j];
+                }
+            }
+            setZeroes(arr);
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    cout<<arr[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+        }
