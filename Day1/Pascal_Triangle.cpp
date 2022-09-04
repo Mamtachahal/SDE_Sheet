3ca53d7e2d1f13af22a9554be25b3646b84610e9
Mamta <83857458+Mamtachahal@users.noreply.github.com>
2022-09-04 12:46:34 +0530

    Update Pascal_Triangle.cpp

--- Day1/Pascal_Triangle.cpp
+++ Day1/Pascal_Triangle.cpp
@@ -14,3 +14,35 @@ public:
         
     }
 };
+
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void se(int n) {
+      vector<vector<int>>r(n);
+      for(int i=0;i<n;i++)
+        { 
+            r[i].resize(i+1);
+            r[i][0]=r[i][i]=1;
+            for(int j=1;j<i;j++)
+                r[i][j]=r[i-1][j-1]+r[i-1][j];
+        }
+        
+          for(int i=0;i<r.size();i++)
+            {
+                for(int j=0;j<r[i].size();j++)
+                {
+                    cout<<r[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+    }
+ 
+        int main(){
+            int n=5;
+            se(n);
+            
+        }
